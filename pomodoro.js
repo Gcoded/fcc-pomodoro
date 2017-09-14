4de6c2a360d85d478967310ea28b29576baf7860
Godfrey <godfreyjr@gmail.com>
2017-09-13 22:37:26 -0500

    Add event handler to adjust time settings

--- pomodoro.js
+++ pomodoro.js
@@ -1,11 +1,39 @@
 
-var timeArray = $('#timer').text().split(':');
-var minutes = parseInt(timeArray[0]);
-var seconds = parseInt(timeArray[1]);
-var totalSecs = (minutes * 60) + (seconds * 1)
+var setTime = 0;
+var timeArray = [];
+var minutes = 0;
+var seconds = 0;
+var totalSecs = 0;
 var timer1;
 
+$('.adjust').click(function(event) {
+    var parentID = event.target.parentNode.id;
+    var adjustment = event.target.textContent;
+    if (parentID === 'workDiv') {
+        setTime = parseInt($('#workTime').text());
+        if(adjustment === '+')
+            setTime++;
+        else
+            setTime--;
+        $('#workTime').text(setTime + " minutes");
+        $('#timer').text(setTime + ':00');
+    }
+    else {
+        setTime = parseInt($('#breakTime').text());
+        if(adjustment === '+')
+            setTime++;
+        else
+            setTime--;
+        $('#breakTime').text(setTime + " minutes");
+    }
+});
+
 function startTimer() {
+    timeArray = $('#timer').text().split(':');
+    minutes = parseInt(timeArray[0]);
+    seconds = parseInt(timeArray[1]);
+    totalSecs = (minutes * 60) + (seconds * 1);
+
     timer1 = setInterval(function() {
         if(totalSecs > 0) {
             if(seconds === -1) {
