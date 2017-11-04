243c1aba8dfaa2b2ba8296869880107d0c3cde5b
Godfrey <godfreyjr@gmail.com>
2017-11-04 14:59:56 -0500

    Change sound, adjust buttons, and finalize styling

--- pomodoro.js
+++ pomodoro.js
@@ -6,40 +6,51 @@ var totalSecs = 0;
 var timer;
 var timerRunning = false;
 var working = true;
+var workColor = '#BAD1F2';
+var breakColor = '#A1D1C4';
+var backgroundColor = '';
 
 $('.adjust').click(function(event) {
-    if(!timerRunning) {
+    if (!timerRunning) {
         var parentID = event.target.parentNode.id;
         var adjustment = event.target.id;
         var setTime = 0;
 
         if (parentID === 'workDiv') {
             setTime = parseInt($('#workTime').text());
-            if(adjustment === 'increase')
+            if (adjustment === 'increase')
                 setTime++;
-            else if(setTime > 1)
+            else if (setTime > 1)
                 setTime--;
 
-            $('#workTime').text(setTime + ' minutes');
-            if(working)     // only change timer if during work time
+            if (setTime === 1)
+                $('#workTime').text(setTime + ' minute');
+            else
+                $('#workTime').text(setTime + ' minutes');
+
+            if (working)     // only change timer if during work time
                 $('#timer').text(setTime + ':00');
         }
         else {
             setTime = parseInt($('#breakTime').text());
-            if(adjustment === 'increase')
+            if (adjustment === 'increase')
                 setTime++;
-            else if(setTime > 1)
+            else if (setTime > 1)
                 setTime--;
 
-            $('#breakTime').text(setTime + ' minutes');
-            if(!working)     // only change timer if during break time
+            if (setTime === 1)
+                $('#breakTime').text(setTime + ' minute');
+            else
+                $('#breakTime').text(setTime + ' minutes');
+
+            if (!working)     // only change timer if during break time
                 $('#timer').text(setTime + ':00');
         }
     }
 });
 
 function startTimer() {
-    if(!timerRunning) {
+    if (!timerRunning) {
         timeArray = $('#timer').text().split(':');
         minutes = parseInt(timeArray[0]);
         seconds = parseInt(timeArray[1]);
@@ -52,30 +63,35 @@ function startTimer() {
 
             displayNumbersAsTime();
 
-            if(totalSecs === 0) {
+            if (totalSecs === 0) {
                 sound.play();
                 working = !working;
+                if (working)
+                    backgroundColor = workColor;
+                else
+                    backgroundColor = breakColor;
                 $('#message').css('visibility', 'hidden');
+                $('body').css('background-color', backgroundColor);
             }
-            if(totalSecs <= -1 && totalSecs >= -3) {    //show message for 3 secs
-                if(working)
-                    $('#timer').text('Back to work!');
+            if (totalSecs <= -1 && totalSecs >= -3) {    //show message for 3 secs
+                if (working)
+                    $('#timer').text('Back to work...');
                 else
                     $('#timer').text('Relax!');
             }
-            if(totalSecs === -4) {          //After 3 second break, set timer for next activity
-                if(working) {
+            if (totalSecs === -4) {          //After 3 second break, set timer for next activity
+                if (working) {
                     minutes = parseInt($('#workTime').text());
                     seconds = 0;
                     totalSecs = minutes * 60;
-                    $('#message').text('Grindin...');
+                    $('#message').text('#GrindTime');
                     $('#timer').text(minutes + ':00');
                 }
                 else {
                     minutes = parseInt($('#breakTime').text());
                     seconds = 0;
                     totalSecs = minutes * 60;
-                    $('#message').text('Chillin...');
+                    $('#message').text('#OnChill');
                     $('#timer').text(minutes + ':00');
                 }
                 $('#message').css('visibility', 'visible');
@@ -86,31 +102,31 @@ function startTimer() {
 
         }, 1000);
     }
-}
-
-function pauseTimer() {
-    clearInterval(timer);
-    timerRunning = false;
-    $('#message').css('visibility', 'hidden');
+    else {
+        clearInterval(timer);
+        timerRunning = false;
+        $('#message').css('visibility', 'hidden');
+    }
 }
 
 function resetTimer() {
     clearInterval(timer);
     timerRunning = false;
     working = true;
-    $('#message').text('Grindin...');
+    $('#message').text('#GrindTime');
     $('#message').css('visibility', 'hidden');
+    $('body').css('background-color', workColor);
     minutes = parseInt($('#workTime').text());
     totalSecs = (minutes * 60);
     $('#timer').text(minutes + ':00');
 }
 
 function displayNumbersAsTime() {
-    if(seconds === -1) {
+    if (seconds === -1) {
         seconds = 59;
         minutes--;
     }
-    if(seconds < 10) {
+    if (seconds < 10) {
         seconds = '0' + seconds;
     }
     $('#timer').text(minutes + ':' + seconds);
